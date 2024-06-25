8ea6680ff07584862aebd2e0f47584917ecdfead
Samuel Baruch <shamuelbaruch@gmail.com>
2024-06-25 14:50:00 -0300

    clean up property page

---
+++ components/PropertyBookmarkButton.jsx
@@ -0,0 +1,16 @@
+import { FaBookmark } from "react-icons/fa";
+
+const PropertyBookmarkButton = () => {
+  return (
+    <div className="my-3">
+      <button
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full
+  flex items-center justify-center"
+      >
+        <FaBookmark className="mr-2" />
+        Bookmark Property
+      </button>
+    </div>
+  );
+};
+export default PropertyBookmarkButton;
