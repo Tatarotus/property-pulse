9f4366e7a719edb12d58ac3ea8d6d913375626fd
Samuel Baruch <shamuelbaruch@gmail.com>
2024-06-30 13:16:12 -0300

    handle user delete message

--- components/Message.jsx
+++ components/Message.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 
 const Message = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const handleReadClick = async () => {
     try {
@@ -22,7 +23,23 @@ const Message = ({ message }) => {
     }
   };
 
-  return (
+  const handleDeleteClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, {
+        method: "DELETE",
+      });
+
+      if (res.status === 200) {
+        // Delete was successful
+        setIsDeleted(true);
+        toast.success("Message deleted");
+      }
+    } catch (error) {
+      console.error("Error deleting message:", error);
+    }
+  };
+
+  return isDeleted ? null : (
     <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
       {!isRead && (
         <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">
@@ -66,7 +83,10 @@ const Message = ({ message }) => {
       >
         {isRead ? "Mark as New" : "Mark Read"}
       </button>
-      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+      <button
+        onClick={handleDeleteClick}
+        className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md"
+      >
         Delete
       </button>
     </div>
